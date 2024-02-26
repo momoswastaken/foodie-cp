3fe7b146f3cf9a8e8e9eec6ef8f1229d5c22ebe6
momoswastaken <parkarmuhammad275@gmail.com>
2024-02-26 19:26:49 +0530

    cp

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div style={{ padding: "16px 0px", backgroundColor: "rgb(255, 257, 257)", textAlign: 'center', position: 'fixed', width: '100%', height: '60px', top: '0', left: '0', zIndex: '999' }}>
+            {/* Footer content */}
+        </div>
+    )
+}
